eb43230ccccb76456750fa235e54b410c99bc8f8
Robin-lee <cmin19991105@gmail.com>
2025-06-20 17:46:31 +0900

    RDBMS

--- 05_다대다관계_연습 .sql
+++ 05_다대다관계_연습 .sql
@@ -1,6 +1,6 @@
 # 고객, 제품, 구매, 구매상세 테이블을 생성
-# 여러 고객은 여러 제품을 구매할 수 있습니다.
-# 한 번의 구매에는 여러 제품이 포함 될 수 있습니다.
+# 한 고객은 여러 번 구매할 수 있습니다. (고객 - 구매)
+# 여러번 의 구매에는 각 각 제품이 포함 될 수 있습니다.(구매 - 구매상세 - 제품)
 # 고객명, 연락처, 제품명, 제품가격, 재고, 구매일 , 구매한 제품 갯수
 
  #db model 데이터 베이스 생성
@@ -16,28 +16,30 @@ customer_name VARCHAR(100) NOT NULL,
 customer_number VARCHAR(11)  NULL
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS tbl_product(
-product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-product_name VARCHAR(100) NOT NULL,
-stock INT NOT NULL,
-price INT NOT NULL
-
-) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS tbl_purchase(
 purchase_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 purchased_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-cust_id INT NOT NULL,
-FOREIGN KEY (cust_id) REFERENCES tbl_customer(customer_id) ON DELETE CASCADE
+customer_id INT NOT NULL,
+FOREIGN KEY (customer_id) REFERENCES tbl_customer(customer_id) ON DELETE CASCADE
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS tbl_product(
+product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+product_name VARCHAR(100) NOT NULL,
+stock INT NOT NULL,
+price DECIMAL(10,2) NOT NULL
+) ENGINE = InnoDB;
+
+
 
 CREATE TABLE IF NOT EXISTS tbl_purchasedetail(
 purchasedetail_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-ea INT NOT NULL,
-prod_id INT NOT NULL,
-purch_id INT NOT NULL,
-FOREIGN KEY (prod_id) REFERENCES tbl_product(product_id) ON DELETE CASCADE,
-FOREIGN KEY (purch_id) REFERENCES tbl_purchase(purchase_id) ON DELETE CASCADE
+ea INT NOT NULL DEFAULT 1,
+product_id INT NOT NULL,
+purchase_id INT NOT NULL,
+FOREIGN KEY (product_id) REFERENCES tbl_product(product_id) ON DELETE CASCADE,
+FOREIGN KEY (purchase_id) REFERENCES tbl_purchase(purchase_id) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
 
